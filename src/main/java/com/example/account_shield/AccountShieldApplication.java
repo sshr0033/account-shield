e5ba87971b7e53a117f6da408f62b68f7694e37c
shreyashrestha <sshr0033@student.monash.edu>
2026-06-04 01:10:52 +1000

    First Initial push

---
+++ src/main/java/com/example/account_shield/AccountShieldApplication.java
@@ -0,0 +1,13 @@
+package com.example.account_shield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountShieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountShieldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/account_shield/AccountShieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.account_shield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountShieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
